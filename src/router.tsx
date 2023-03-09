a664078d808dbf345212b833271c1e6d9b66932d
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2023-03-09 17:05:29 +0100

    Replace routes config with object style

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { Route, createRoutesFromElements, redirect } from 'react-router';
+import { redirect } from 'react-router';
 import { createBrowserRouter } from 'react-router-dom';
 
 import { DataPage } from './pages/DataPage';
@@ -7,14 +7,31 @@ import { TermsPage } from './pages/TermsPage';
 import { MapPage } from './pages/map/MapPage';
 import { RootErrorRoute, RootRoute } from './pages/root';
 
-export const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<RootRoute />} errorElement={<RootErrorRoute />}>
-      <Route path="/" element={<IntroPage />} />
-      <Route path="/view" loader={() => redirect('/view/hazard')} />
-      <Route path="/view/:view" element={<MapPage />} />
-      <Route path="/data" element={<DataPage />} />
-      <Route path="/terms-of-use" element={<TermsPage />} />
-    </Route>,
-  ),
-);
+export const router = createBrowserRouter([
+  {
+    element: <RootRoute />,
+    errorElement: <RootErrorRoute />,
+    children: [
+      {
+        path: '/',
+        element: <IntroPage />,
+      },
+      {
+        path: '/view',
+        loader: () => redirect('/view/hazard'),
+      },
+      {
+        path: '/view/:view',
+        element: <MapPage />,
+      },
+      {
+        path: '/data',
+        element: <DataPage />,
+      },
+      {
+        path: '/terms-of-use',
+        element: <TermsPage />,
+      },
+    ],
+  },
+]);
