9f1f42f976e937d80920ba0eef91cd8b26dae239
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2023-03-09 16:20:40 +0100

    Install dev version of autopkg client

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,5 +1,11 @@
+import { AutoPkgClient } from '@nismod/irv-autopkg-client';
+
 import { ApiClient } from './lib/api-client';
 
 export const apiClient = new ApiClient({
   BASE: '/api',
+});
+
+export const autopkgClient = new AutoPkgClient({
+  BASE: '/extract',
 });
