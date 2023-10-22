60988e5b43ca92c3f4aee2426471a3fde4a2cd34
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2023-10-22 15:04:22 +0200

    Add more deck prop merge strategies

--- src/lib/deck/layers/base.ts
+++ src/lib/deck/layers/base.ts
@@ -2,19 +2,29 @@ import { BitmapLayer, GeoJsonLayer, MVTLayer, TileLayer } from 'deck.gl/typed';
 
 import { ConfigTree } from '@/lib/nested-config/config-tree';
 import { flattenConfig } from '@/lib/nested-config/flatten-config';
-import { appendValue, mergeObjects, mergeValue } from '@/lib/nested-config/merge-objects';
+import {
+  appendValue,
+  composeAppendValue,
+  makeObjectsMerger,
+} from '@/lib/nested-config/merge-objects';
+
+const mergeUpdateTriggers = makeObjectsMerger({
+  renderSubLayers: appendValue,
+});
 
 /**
  * A set of strategies for merging compound layer properties
  * specific to Deck.GL
  */
-const deckPropsMergeStrategies = {
-  // merge updateTriggers objects with Object.assign() semantics
-  updateTriggers: mergeValue,
+const mergeDeckProps = makeObjectsMerger({
+  updateTriggers: mergeUpdateTriggers,
 
   // concatenate extensions arrays
   extensions: appendValue,
-};
+
+  // concatenate the results of all `renderSubLayer` functions
+  renderSubLayers: composeAppendValue,
+});
 
 /**
  * A function to merge multiple props objects passed to a Deck.GL layer.
@@ -23,14 +33,14 @@ const deckPropsMergeStrategies = {
  * - nested arrays are flattened
  * - compound props (currently only `updateTriggers`) are merged instead of overwritten
  */
-function mergeDeckProps(...props: ConfigTree<object>): any {
+function processDeckProps(...props: ConfigTree<object>): any {
   const flattenedProps = flattenConfig(props);
 
-  return mergeObjects(flattenedProps, deckPropsMergeStrategies);
+  return mergeDeckProps(...flattenedProps);
 }
 
 function wrap(deckClass) {
-  return (...props) => new deckClass(mergeDeckProps(props));
+  return (...props) => new deckClass(processDeckProps(props));
 }
 
 export const mvtLayer = wrap(MVTLayer);
