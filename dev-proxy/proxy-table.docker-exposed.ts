3d7f2fa843680faef8644b3ba921cf7720e582e7
Tom Russell <tom.russell@ouce.ox.ac.uk>
2024-08-06 15:59:06 +0100

    Add variations on local dev host/port setup

--- dev-proxy/proxy-table.docker-exposed.ts
+++ dev-proxy/proxy-table.docker-exposed.ts
@@ -3,24 +3,23 @@
  * ./dev-proxy/proxy-table.ts and conforms to the syntax described here:
  * https://vitejs.dev/config/server-options.html#server-proxy
  *
- * The hostnames that follow are docker service names, and so to resolve to
- * containers. If using this setup, the vite server must belong to the docker
- * network containing the backend services (see ./README.md).
+ * The ports that follow are those exposed by a development docker-compose
+ * setup, or could be run locally on the host if preferred (see ./README.md).
  */
 
 export const devProxy = {
   '/vector': {
-    target: 'http://vector-tileserver:8080',
+    target: 'http://localhost:8080',
     changeOrigin: true,
     rewrite: (path) => path.replace(/^\/vector/, ''),
   },
   '/api': {
-    target: 'http://backend:8888',
+    target: 'http://localhost:8888',
     changeOrigin: true,
     rewrite: (path) => path.replace(/^\/api/, ''),
   },
   '/extract': {
-    target: 'http://irv-autopkg-api:8000',
+    target: 'http:/localhost:8000',
     changeOrigin: true,
     rewrite: (path) => path.replace(/^\/extract/, ''),
   },
