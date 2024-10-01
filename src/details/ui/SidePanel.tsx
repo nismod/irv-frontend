cbf764f4fd340ea8aa7babebb5a5912facb505a9
Tom Russell <tom.russell@ouce.ox.ac.uk>
2024-10-01 11:59:55 +0100

    Silence type warning on SidePanel props

--- src/details/ui/SidePanel.tsx
+++ src/details/ui/SidePanel.tsx
@@ -2,12 +2,10 @@ import { BoxProps, Paper } from '@mui/material';
 import { Box } from '@mui/system';
 import { FC } from 'react';
 
-export const SidePanel: FC<BoxProps> = ({ children, ...props }) => {
+export const SidePanel: FC<BoxProps> = ({ children }) => {
   return (
     <Paper>
-      <Box px={3} py={2} {...props}>
-        {children}
-      </Box>
+      <Box sx={{ px: 3, py: 2, position: 'relative' }}>{children}</Box>
     </Paper>
   );
 };
