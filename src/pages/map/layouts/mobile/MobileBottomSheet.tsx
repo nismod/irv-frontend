9d3bdf5f003e49b924de58b3e91eecaa57d83222
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2025-03-08 18:09:55 +0100

    Replace react-spring-bottom-sheet with vaul

--- src/pages/map/layouts/mobile/MobileBottomSheet.tsx
+++ src/pages/map/layouts/mobile/MobileBottomSheet.tsx
@@ -1,47 +1,93 @@
 import { TabContext } from '@mui/lab';
-import { BottomNavigation, Box } from '@mui/material';
-import { FC, useState } from 'react';
-import { BottomSheet } from 'react-spring-bottom-sheet';
+import { BottomNavigation, Box, Paper } from '@mui/material';
+import { FC, useEffect, useRef, useState } from 'react';
+import { Drawer } from 'vaul';
 
 import { ContentWatcherScope } from '@/lib/mobile-tabs/content-watcher';
 import { NonUnmountingTabPanel } from '@/lib/mobile-tabs/NonUnmountingTabPanel';
 import { TabNavigationAction } from '@/lib/mobile-tabs/TabNavigationAction';
 
-import { globalStyleVariables } from '@/theme';
-
 import { MobileTabContentWatcher, mobileTabHasContentState } from './tab-has-content';
 import { TabConfig } from './tabs-config';
 
-import './bottom-sheet.css';
+const bottomNavigationHeightPx = 56;
+const handleMarginBlockPx = 15;
+const bottomSheetHandleHeightPx = 2 * handleMarginBlockPx + 5;
 
+type SnapPoint = string | number;
 export const MobileBottomSheet: FC<{ tabsConfig: TabConfig[] }> = ({ tabsConfig }) => {
   const [bottomTabId, setBottomTabId] = useState(tabsConfig[0]?.id);
 
+  const scrollRef = useRef<HTMLDivElement>(null);
+  usePreventOverscroll(scrollRef);
+
+  const snapPoints: SnapPoint[] = [
+    `${bottomNavigationHeightPx + bottomSheetHandleHeightPx}px`,
+    0.35,
+    0.5,
+    0.9, // calculation not supported here so leave roughly 10% for header
+  ];
+
+  const [activeSnapPoint, setActiveSnapPoint] = useState(snapPoints[1]);
+
+  const scrollAreaHeight = `calc(100dvh - var(--snap-point-height) - ${bottomSheetHandleHeightPx + bottomNavigationHeightPx}px)`;
   return (
-    <BottomSheet
-      open={true}
-      blocking={false}
-      expandOnContentDrag={true}
-      skipInitialTransition={true}
-      snapPoints={({ footerHeight, maxHeight }) => [
-        footerHeight + 38, // magic number to allow the puller/header to be visible at the smallest snap point
-        maxHeight * 0.35,
-        maxHeight * 0.6,
-        maxHeight - globalStyleVariables.navbarHeight - 4,
-      ]}
-      footer={
-        <Box sx={{ cursor: 'default' }}>
+    <Drawer.Root
+      defaultOpen={true}
+      modal={false}
+      dismissible={false}
+      snapPoints={snapPoints}
+      activeSnapPoint={activeSnapPoint}
+      setActiveSnapPoint={setActiveSnapPoint}
+      snapToSequentialPoint={false}
+      handleOnly={true}
+    >
+      <Drawer.Portal>
+        <Drawer.Content asChild={true}>
+          <Box position="fixed" zIndex={1001} bottom={0} left={0} right={0}>
+            <Paper
+              elevation={3}
+              square={true}
+              sx={{
+                borderTopLeftRadius: '24px',
+                borderTopRightRadius: '24px',
+              }}
+            >
+              <Box pt={0.1} height="100dvh">
+                <Drawer.Handle
+                  style={{ marginBlock: `${handleMarginBlockPx}px`, width: '150px' }}
+                />
+                <div
+                  style={{
+                    height: scrollAreaHeight,
+                    minHeight: scrollAreaHeight,
+                    maxHeight: scrollAreaHeight,
+                  }}
+                >
+                  <div
+                    ref={scrollRef}
+                    style={{
+                      overflowY: 'scroll',
+                      height: '100%',
+                      overscrollBehavior: 'contain',
+                    }}
+                  >
+                    <MobileBottomSheetTabContent tabsConfig={tabsConfig} selectedId={bottomTabId} />
+                  </div>
+                </div>
+              </Box>
+            </Paper>
+          </Box>
+        </Drawer.Content>
+        <Box position="fixed" bottom={0} zIndex={6000} width="100%" sx={{ cursor: 'default' }}>
           <MobileBottomSheetTabNavigation
             tabsConfig={tabsConfig}
             selectedId={bottomTabId}
             setSelectedId={setBottomTabId}
           />
         </Box>
-      }
-      header={<Box height={10} />}
-    >
-      <MobileBottomSheetTabContent tabsConfig={tabsConfig} selectedId={bottomTabId} />
-    </BottomSheet>
+      </Drawer.Portal>
+    </Drawer.Root>
   );
 };
 
@@ -86,4 +132,39 @@ const MobileBottomSheetTabContent: FC<{
       ))}
     </TabContext>
   );
+};
+
+/** Used to prevent pull-to-refresh gesture on mobile drawer when the drawer content is not scrollable
+ * (because in those cases overscrollBehavior CSS property doesn't work)
+ */
+export const usePreventOverscroll = (scrollRef: React.RefObject<HTMLElement>) => {
+  useEffect(() => {
+    const scrollContainer = scrollRef.current;
+    if (!scrollContainer) return;
+
+    let startY = 0;
+
+    const handleTouchStart = (e: TouchEvent) => {
+      startY = e.touches[0].clientY;
+    };
+
+    const handleTouchMove = (e: TouchEvent) => {
+      const isAtTop = scrollContainer.scrollTop === 0;
+
+      const currentY = e.touches[0].clientY;
+      const isSwipingDown = currentY > startY; // User is pulling down
+
+      if (isAtTop && isSwipingDown) {
+        e.preventDefault(); // Fully block pull-to-refresh
+      }
+    };
+
+    scrollContainer.addEventListener('touchstart', handleTouchStart, { passive: false });
+    scrollContainer.addEventListener('touchmove', handleTouchMove, { passive: false });
+
+    return () => {
+      scrollContainer.removeEventListener('touchstart', handleTouchStart);
+      scrollContainer.removeEventListener('touchmove', handleTouchMove);
+    };
+  }, [scrollRef]);
 };
