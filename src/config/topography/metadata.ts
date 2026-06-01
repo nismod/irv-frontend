b536bfd51be6724221780790ba71f491ba3fb588
Tom Russell <tom.russell@ouce.ox.ac.uk>
2026-06-01 14:21:44 +0100

    Revise topography metadata

--- src/config/topography/metadata.ts
+++ src/config/topography/metadata.ts
@@ -22,9 +22,9 @@ export const DEM_RASTER_VALUE_LABELS: ValueLabel<TopographyType>[] = [
 export const TOPOGRAPHY_LAYER_METADATA = [
   {
     id: 'dem',
-    title: 'Topography',
+    title: 'Topography (Hengl/MERIT)',
     description:
-      'DEM derivatives computed using SAGA GIS at 250m and using MERIT DEM (Yamazaki et al., 2017) as input. Antarctica is not included. MERIT DEM was first reprojected to 6 global tiles based on the Equi7 grid system (Bauer-Marschallinger et al. 2014) and then these were used to derive all DEM derivatives. To access original DEM tiles please refer to the MERIT DEM download page: http://hydro.iis.u-tokyo.ac.jp/~yamadai/MERIT_DEM/.',
+      'DEM derivatives computed using SAGA GIS at 250m and using MERIT DEM (Yamazaki et al., 2017) as input. Antarctica is not included. MERIT DEM was first reprojected to 6 global tiles based on the Equi7 grid system (Bauer-Marschallinger et al. 2014) and then these were used to derive all DEM derivatives. To access original DEM tiles please refer to the MERIT DEM download page: https://global-hydrodynamics.github.io/MERIT_DEM/',
     risk_data_type: ['exposure'],
     spatial: GLOBAL_SPATIAL,
     publisher: { name: 'Tomislav Hengl' },
@@ -44,7 +44,7 @@ export const TOPOGRAPHY_LAYER_METADATA = [
       sources: [
         {
           id: 'source_global_dem_derivatives_merit_dem',
-          name: 'Tomislav Hengl. (2018). Global DEM derivatives at 250m, 1 km and 2 km based on the MERIT DEM (1.0) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.1447210.',
+          name: 'Tomislav Hengl. (2018). Global DEM derivatives at 250m, 1 km and 2 km based on the MERIT DEM (1.0) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.1447210',
           url: 'https://doi.org/10.5281/zenodo.1447210',
           type: 'dataset',
           risk_data_type: ['exposure'],
