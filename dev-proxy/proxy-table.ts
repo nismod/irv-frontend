191e5789f70a7bc89587bd709d566cd77f494205
Tom Russell <tom.russell@ouce.ox.ac.uk>
2026-06-24 18:54:36 +0100

    Include empty devProxy to satisfy vite build

---
+++ dev-proxy/proxy-table.ts
@@ -0,0 +1,10 @@
+/**
+ * To set up a development proxy, edit the file dev-proxy/proxy-table.ts
+ * You can copy and rename one of the included examples.
+ *
+ * To configure the vite development proxy server, ensure this file is at
+ * ./dev-proxy/proxy-table.ts and conforms to the syntax described here:
+ * https://vitejs.dev/config/server-options.html#server-proxy
+ */
+
+export const devProxy = {};
